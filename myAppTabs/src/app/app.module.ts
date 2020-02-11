59a09ed9df45164ef829825db65e8c4e3cc148d7
Antonio Aguiar <antonioaguiar@gmail.com>
2020-02-11 01:16:07 +0000

    colors

--- myAppTabs/src/app/app.module.ts
+++ myAppTabs/src/app/app.module.ts
@@ -11,7 +11,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { DragulaModule } from 'ng2-dragula';
-
+ 
 
 @NgModule({
   declarations: [AppComponent],
